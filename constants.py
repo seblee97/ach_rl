e9e85f958e2647fa832fc00b1edc71990ce4833b
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-10-15 16:13:13 +0100

    added config constants

--- constants.py
+++ constants.py
@@ -1,3 +1,28 @@
 class Constants:
 
     EPSILON_GREEDY = "epsilon_greedy"
+
+    # config
+    SIZE = "size"
+    LIVING_REWARD = "living_reward"
+    NO_OP_PENALTY = "no_op_penalty"
+    STARTING_POSITION = "starting_position"
+    REWARD_POSITION = "reward_position"
+    ENVIRONMENT = "environment"
+    MINIGRID = "minigrid"
+    EPISODE_TIMEOUT = "episode_timeout"
+    EPSILON = "epsilon"
+    POLICY = "policy"
+    EPSILON_GREEDY = "epsilon_greedy"
+    Q_LEARNER = "q_learner"
+    ALPHA = "alpha"
+    GAMMA = "gamma"
+    LEARNER = "learner"
+    TRAINING = "training"
+    NUM_EPISODES = "num_episodes"
+    EXPERIMENT_NAME = "experiment_name"
+    SEED = "seed"
+
+    RESULTS = "results"
+    CHECKPOINT_PATH = "checkpoint_path"
+    EXPERIMENT_TIMESTAMP = "experiment_timestamp"
