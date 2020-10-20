47ac4bba7bce987fd2d658180bfd1be0ed0ad702
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-10-20 10:58:47 +0100

    changed state-action data structures to np arrays (i.e. matrices) since mapping is lost and we can only index by ids, setup a separate mapping from state to state-id for better interpretability

--- learners/backward_sarsa_lambda.py
+++ learners/backward_sarsa_lambda.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Tuple
+from typing import List, Optional, Tuple
 
 import constants
 import numpy as np
@@ -20,40 +20,37 @@ class BackwardSARSALambda(base_learner.BaseLearner):
     ):
         self._action_space = action_space
         self._state_space = state_space
+
+        self._state_id_mapping = {state: i for i, state in enumerate(self._state_space)}
+
         self._state_action_values = self._initialise_values(
             initialisation_strategy=initialisation_strategy
         )
         self._state_action_eligibility_traces = self._initialise_values(
             initialisation_strategy=constants.Constants.ZEROS
         )
+
         self._learning_rate = learning_rate
         self._gamma = gamma
         self._visitation_penalty = visitation_penalty
         self._epsilon = epsilon
         self._trace_lambda = trace_lambda
 
-    def _initialise_values(self, initialisation_strategy: str) -> Dict[int, float]:
+    def _initialise_values(self, initialisation_strategy: str) -> np.ndarray:
         """Initialise values for each state, action pair in state-action space.
 
         Args:
             initialisation_strategy: name of method used to initialise.
 
         Returns:
-            initial_values: dictionary containing state-action id / value mapping.
+            initial_values: matrix containing state-action id / value mapping.
         """
         if initialisation_strategy == constants.Constants.RANDOM:
-            return {
-                state: np.random.rand(len(self._action_space))
-                for state in self._state_space
-            }
+            return np.random.rand(len(self._state_space), len(self._action_space))
         elif initialisation_strategy == constants.Constants.ZEROS:
-            return {
-                state: np.zeros(len(self._action_space)) for state in self._state_space
-            }
+            return np.zeros((len(self._state_space), len(self._action_space)))
         elif initialisation_strategy == constants.Constants.ONES:
-            return {
-                state: np.ones(len(self._action_space)) for state in self._state_space
-            }
+            return np.ones((len(self._state_space), len(self._action_space)))
 
     def _max_state_action_value(self, state: Tuple[int, int]) -> float:
         """Find highest value in given state.
@@ -64,7 +61,8 @@ class BackwardSARSALambda(base_learner.BaseLearner):
         Returns:
             value: corresponding highest value.
         """
-        return np.amax(self._state_action_values[state])
+        state_id = self._state_id_mapping[state]
+        return np.amax(self._state_action_values[state_id])
 
     def _greedy_action(self, state: Tuple[int, int]) -> int:
         """Find action with highest value in given state.
@@ -75,7 +73,8 @@ class BackwardSARSALambda(base_learner.BaseLearner):
         Returns:
             action: action with highest value in state given.
         """
-        return np.argmax(self._state_action_values[state])
+        state_id = self._state_id_mapping[state]
+        return np.argmax(self._state_action_values[state_id])
 
     def select_target_action(self, state: Tuple[int, int]) -> int:
         """Select action according to target policy, i.e. policy being learned.
@@ -116,7 +115,7 @@ class BackwardSARSALambda(base_learner.BaseLearner):
         state: Tuple[int, int],
         action: int,
         reward: float,
-        new_state: Tuple[int, int],
+        next_state: Tuple[int, int],
         next_action: int,
     ) -> None:
         """Update state-action values.
@@ -132,11 +131,14 @@ class BackwardSARSALambda(base_learner.BaseLearner):
             state: state before update.
             action: action taken by agent.
             reward: scalar reward received from environment.
-            new_state: next state.
+            next_state: next state.
             next_action: action taken by agent one step later.
         """
-        current_state_action_value = self._state_action_values[state][action]
-        current_next_state_action_value = self._state_action_values[new_state][
+        state_id = self._state_id_mapping[state]
+        next_state_id = self._state_id_mapping[next_state]
+
+        current_state_action_value = self._state_action_values[state_id][action]
+        current_next_state_action_value = self._state_action_values[next_state_id][
             next_action
         ]
 
@@ -148,17 +150,15 @@ class BackwardSARSALambda(base_learner.BaseLearner):
         )
 
         # increment trace associated with visited state, action pair
-        self._state_action_eligibility_traces[state][action] += 1
+        self._state_action_eligibility_traces[state_id][action] += 1
 
-        # decay eligibility traces and update other state-action values
-        for state in self._state_space:
-            change = (
-                self._learning_rate
-                * delta
-                * self._state_action_eligibility_traces[state]
-                - self._visitation_penalty
-            )
-            self._state_action_values[state] += change
-            self._state_action_eligibility_traces[state] *= (
-                self._gamma * self._trace_lambda
-            )
+        # decay eligibility traces
+        self._state_action_values += (
+            self._learning_rate * delta * self._state_action_eligibility_traces
+        )
+
+        # update other state-action values
+        self._state_action_eligibility_traces *= self._gamma * self._trace_lambda
+
+        # apply visitation penalty
+        self._state_action_values[state_id][action] -= self._visitation_penalty
