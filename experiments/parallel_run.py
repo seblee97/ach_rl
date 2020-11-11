da80a29250ad8625f79c9e359ebab2bd44b1b5d0
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-11-11 15:38:52 +0000

    increase range of default vps

--- experiments/parallel_run.py
+++ experiments/parallel_run.py
@@ -27,7 +27,9 @@ parser.add_argument(
     help="run in 'parallel' or 'serial'",
 )
 parser.add_argument("--config", metavar="-C", default="config.yaml")
-parser.add_argument("--penalties", metavar="-P", default=[0, 0.001, 0.002])
+parser.add_argument(
+    "--penalties", metavar="-P", default=[0, 0.001, 0.002, 0.003, 0.004, 0.005]
+)
 parser.add_argument("--seeds", metavar="-S", default=range(5))
 
 args = parser.parse_args()
