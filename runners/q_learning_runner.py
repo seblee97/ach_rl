e4481e1013a2e1b5386f63bfdb6b6ae9d7ea9956
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-11-17 16:18:09 +0000

    train episode gets spieode numbers from which vp can be computed and passed as arg to learner step

--- runners/q_learning_runner.py
+++ runners/q_learning_runner.py
@@ -23,12 +23,14 @@ class QLearningRunner(base_runner.BaseRunner):
             epsilon=config.epsilon,
             learning_rate=config.learning_rate,
             gamma=config.discount_factor,
-            visitation_penalty=config.visitation_penalty,
         )
         return learner
 
-    def _train_episode(self) -> Tuple[float, int]:
+    def _train_episode(self, episode: int) -> Tuple[float, int]:
         """Perform single training loop.
+
+        Args:
+            episode: index of episode
 
         Returns:
             episode_reward: scalar reward accumulated over episode.
@@ -36,13 +38,17 @@ class QLearningRunner(base_runner.BaseRunner):
         """
         episode_reward = 0
 
+        visitation_penalty = self._visitation_penalty(episode)
+
+        print(episode, visitation_penalty)
+
         self._environment.reset_environment(train=True)
         state = self._environment.agent_position
 
         while self._environment.active:
             action = self._learner.select_behaviour_action(state)
             reward, new_state = self._environment.step(action)
-            self._learner.step(state, action, reward, new_state)
+            self._learner.step(state, action, reward, new_state, visitation_penalty)
             state = new_state
             episode_reward += reward
 
