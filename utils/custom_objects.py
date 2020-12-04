c2b71602a0b4516a7d87d986aac1ca19220e5765
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-12-04 11:29:33 +0000

    named tuple for transition

---
+++ utils/custom_objects.py
@@ -0,0 +1,13 @@
+from collections import namedtuple
+import constants
+
+Transition = namedtuple(
+    typename=constants.Constants.TRANSITION,
+    field_names=[
+        constants.Constants.STATE_ENCODING,
+        constants.Constants.ACTION,
+        constants.Constants.REWARD,
+        constants.Constants.NEXT_STATE_ENCODING,
+        constants.Constants.ACTIVE,
+    ],
+)
