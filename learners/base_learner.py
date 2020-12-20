c4a3fbe105924cef63f32755a4d43d62cdae5dc4
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-12-20 11:38:17 +0000

    base learner abstract methods

--- learners/base_learner.py
+++ learners/base_learner.py
@@ -1,8 +1,22 @@
 import abc
 
+from typing import Any
+
 
 class BaseLearner(abc.ABC):
     """Base class for learners."""
 
     def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def eval(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def train(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def select_target_action(self, state: Any) -> None:
         pass
