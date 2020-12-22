dde4d854b6d2dbb418764d2b741e7b7f60f49748
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-12-22 14:31:42 +0000

    add state action values as property of ensemble learner - here interpretation is the mean over ensembel

--- learners/ensemble_learners/sample_greedy_ensemble_learner.py
+++ learners/ensemble_learners/sample_greedy_ensemble_learner.py
@@ -6,6 +6,19 @@ from learners.ensemble_learners import ensemble_learner
 
 
 class SampleGreedyEnsemble(ensemble_learner.EnsembleLearner):
+    @property
+    def state_action_values(self):
+        all_state_action_values = [
+            learner._state_action_values for learner in self._learner_ensemble
+        ]
+        mean_values = np.mean(all_state_action_values, axis=0)
+
+        # mappings for all learners are identical
+        return {
+            self._learner_ensemble[0].id_state_mapping[i]: value
+            for i, value in enumerate(mean_values)
+        }
+
     def select_target_action(self, state: Tuple[int, int]) -> int:
         all_state_action_values = [
             learner.state_action_values for learner in self._learner_ensemble
