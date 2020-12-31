7460bdce2e97f048c44a648b63c199230e652908
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-12-31 12:03:43 +0000

    add set devie method to experiment utils from run file

--- utils/experiment_utils.py
+++ utils/experiment_utils.py
@@ -2,7 +2,11 @@ import datetime
 import os
 import time
 from typing import Optional
+
 import torch
+
+import constants
+from experiments import ach_config
 
 
 def set_random_seeds(seed: int) -> None:
@@ -15,6 +19,29 @@ def set_random_seeds(seed: int) -> None:
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+
+
+def set_device(
+    config: ach_config.AchConfig,
+) -> ach_config.AchConfig:
+    """Establish availability of GPU."""
+    if config.use_gpu:
+        print("Attempting to find GPU...")
+        if torch.cuda.is_available():
+            print("GPU found, using the GPU...")
+            torch.backends.cudnn.deterministic = True
+            torch.backends.cudnn.benchmark = False
+            config.add_property(constants.Constants.USING_GPU, True)
+            experiment_device = torch.device("cuda:{}".format(config.gpu_id))
+        else:
+            print("GPU not found, reverting to CPU")
+            config.add_property(constants.Constants.USING_GPU, False)
+            experiment_device = torch.device("cpu")
+    else:
+        print("Using the CPU")
+        experiment_device = torch.device("cpu")
+    config.add_property(constants.Constants.EXPERIMENT_DEVICE, experiment_device)
+    return config
 
 
 def get_experiment_timestamp() -> str:
