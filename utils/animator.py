4af583a56b4f040e53a5ecb09284024cf66e867b
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-01-08 17:59:39 +0000

    two different methods depending on animation library arg

--- utils/animator.py
+++ utils/animator.py
@@ -2,31 +2,43 @@ from typing import List
 
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+import imageio
 from matplotlib.animation import FuncAnimation
 
 import constants
 
 
-def animate(images: List[np.ndarray], file_name: str, plot_origin: str):
+def animate(
+    images: List[np.ndarray],
+    file_name: str,
+    plot_origin: str,
+    library: str,
+    file_format: str,
+):
     """Create animation from images.
 
     Args:
         images: list of numpy arrays to imshow in animation.
         file_name: name of file under which to save animation.
         plot_origin: orientation of numpy array image.
+        library: method for generating video.
     """
     # add extension to file_name
-    file_name = f"{file_name}.{constants.Constants.ANIMATION_FILE_FORMAT}"
+    file_name = f"{file_name}.{file_format}"
 
-    fig, ax = plt.subplots(figsize=(5, 5))
+    if library == constants.Constants.MATPLOTLIB_ANIMATION:
+        fig, ax = plt.subplots(figsize=(5, 5))
 
-    def update(image):
-        ax.imshow(image, origin=plot_origin)
+        def update(image):
+            ax.imshow(image, origin=plot_origin)
 
-    anim = FuncAnimation(fig, update, frames=images, interval=200)
-    anim.save(
-        file_name,
-        dpi=80,
-        writer="imagemagick",
-    )
-    plt.close()
+        anim = FuncAnimation(fig, update, frames=images[:100], interval=200)
+        anim.save(
+            file_name,
+            dpi=80,
+            writer="imagemagick",
+        )
+        plt.close()
+    elif library == constants.Constants.IMAGEIO:
+        imageio.mimwrite(file_name, images)
