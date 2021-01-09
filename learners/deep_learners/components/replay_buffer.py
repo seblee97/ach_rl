bb20d31b90b93c20f9eee9e3b81013d503af0a30
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-01-09 22:00:21 +0000

    change replay buffer to deque

--- learners/deep_learners/components/replay_buffer.py
+++ learners/deep_learners/components/replay_buffer.py
@@ -1,10 +1,66 @@
 import numpy as np
 from typing import Tuple
+import collections
 from collections import namedtuple
 from utils import custom_objects
 
 
 class ReplayBuffer:
+    """Object for experience replay,
+    used to store experience tuples for off-policy learning."""
+
+    def __init__(self, replay_size: int, state_dim: Tuple) -> None:
+        """Class constructor.
+
+        Args:
+            replay_size: size of buffer.
+            state_dim: dimension of state.
+        """
+        self._buffer = collections.deque(maxlen=replay_size)
+        self._replay_size = replay_size
+
+    def add(self, transition: custom_objects.Transition) -> None:
+        """Add tuple to buffer.
+
+        Args:
+            state: pre-state.
+            action: action.
+            reward: reward.
+            next_state: post-state.
+            active: whether episode is over.
+        """
+        self._buffer.append(transition)
+
+    def sample(self, batch_size: int) -> namedtuple:
+        """Sample experience from buffer.
+
+        Args:
+            batch_size: size of sample.
+
+        Return:
+            sample: experience sample.
+        """
+        sample_indices = np.random.choice(
+            len(self._buffer), size=batch_size, replace=False
+        )
+        (
+            states_sample,
+            actions_sample,
+            rewards_sample,
+            next_states_sample,
+            active_sample,
+        ) = zip(*[self._buffer[idx] for idx in sample_indices])
+
+        return (
+            np.array(states_sample),
+            np.array(actions_sample),
+            np.array(rewards_sample, dtype=np.float32),
+            np.array(next_states_sample),
+            np.array(active_sample, dtype=np.uint8),
+        )
+
+
+class OldReplayBuffer:
     """Object for experience replay,
     used to store experience tuples for off-policy learning."""
 
