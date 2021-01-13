3f6f342054f770c79fd6e7f2bdb70b4bcbc5b79b
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-01-13 09:57:36 +0000

    LSP principle for call method, added short description

--- visitation_penalties/base_visistation_penalty.py
+++ visitation_penalties/base_visistation_penalty.py
@@ -3,12 +3,17 @@ from experiments import ach_config
 
 
 class BaseVisitationPenalty(abc.ABC):
-    """Base class for visitation penalties."""
+    """Base class for visitation penalties.
+
+    Generally calls to this 'penalty' should follow
+    the potential-based reward structure of
+    Ng, Harada, Russell (1999).
+    """
 
     def __init__(self, config: ach_config.AchConfig):
         pass
 
     @abc.abstractmethod
-    def __call__(self, episode: int):
+    def __call__(self, *args, **kwargs):
         """compute penalty at episode"""
         pass
