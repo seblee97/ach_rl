17ac76cba249ac3069db69fa4c751f0d9885cf98
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-02-08 22:55:36 +0000

    separate penalty info method, everyhing vectorsied using np methods

--- visitation_penalties/base_visistation_penalty.py
+++ visitation_penalties/base_visistation_penalty.py
@@ -4,9 +4,9 @@ from typing import Dict
 from typing import List
 from typing import Tuple
 
-import numpy as np
-
 import constants
+import numpy as np
+from utils import decorators
 
 
 class BaseVisitationPenalty(abc.ABC):
@@ -28,59 +28,82 @@ class BaseVisitationPenalty(abc.ABC):
         return self._state_action_values
 
     @state_action_values.setter
-    def state_action_values(self, state_action_values: List[Dict[Tuple[int], float]]):
+    def state_action_values(self, state_action_values: List[Dict[Tuple[int],
+                                                                 float]]):
         self._state_action_values = state_action_values
 
-    def __call__(self, episode, state, action, next_state, *args, **kwargs):
+    @decorators.timer
+    def _get_penalty_info(
+        self,
+        state,
+        action,
+        next_state,
+    ):
         """compute series of uncertainties over states culminating in penalty."""
-        current_state_values = [s[state] for s in self._state_action_values]
-        next_state_values = [s[next_state] for s in self._state_action_values]
+        import pdb
+        pdb.set_trace()
+        current_state_values = np.array(
+            [s[state] for s in self._state_action_values])
+        next_state_values = np.array(
+            [s[next_state] for s in self._state_action_values])
 
         num_actions = len(current_state_values[0])
 
-        current_state_max_action_values = [np.max(s) for s in current_state_values]
-        current_state_max_action_indices = [np.argmax(s) for s in current_state_values]
+        current_state_max_action_values = np.amax(current_state_values, axis=1)
+        current_state_max_action_indices = np.argmax(current_state_values,
+                                                     axis=1)
+
         current_state_max_uncertainty = np.std(current_state_max_action_values)
-        current_state_mean_uncertainty = np.mean(np.std(current_state_values, axis=0))
+        current_state_mean_uncertainty = np.mean(
+            np.std(current_state_values, axis=0))
 
-        current_state_select_uncertainty = np.std(
-            [s[action] for s in current_state_values]
-        )
+        current_state_select_uncertainty = np.std(current_state_values[:,
+                                                                       action])
         current_state_max_action_index_probabilities = np.bincount(
-            current_state_max_action_indices, minlength=num_actions
-        ) / len(current_state_max_action_indices)
+            current_state_max_action_indices,
+            minlength=num_actions) / len(current_state_max_action_indices)
         current_state_policy_entropy = -np.sum(
-            [
-                (probability + self.EPSILON) * np.log(probability + self.EPSILON)
-                for probability in current_state_max_action_index_probabilities
-            ]
-        )
+            (current_state_max_action_index_probabilities + self.EPSILON) *
+            np.log(current_state_max_action_index_probabilities +
+                   self.EPSILON))
 
-        next_state_max_action_values = [np.max(s) for s in next_state_values]
-        next_state_max_action_indices = [np.argmax(s) for s in next_state_values]
+        next_state_max_action_values = np.max(next_state_values, axis=1)
+        next_state_max_action_indices = np.argmax(next_state_values, axis=1)
+
         next_state_max_uncertainty = np.std(next_state_max_action_values)
-        next_state_mean_uncertainty = np.mean(np.std(next_state_values, axis=0))
+        next_state_mean_uncertainty = np.mean(np.std(next_state_values,
+                                                     axis=0))
         next_state_max_action_index_probabilities = np.bincount(
-            next_state_max_action_indices, minlength=num_actions
-        ) / len(current_state_max_action_indices)
+            next_state_max_action_indices,
+            minlength=num_actions) / len(current_state_max_action_indices)
         next_state_policy_entropy = -np.sum(
-            [
-                (probability + self.EPSILON) * np.log(probability + self.EPSILON)
-                for probability in next_state_max_action_index_probabilities
-            ]
-        )
+            (next_state_max_action_index_probabilities + self.EPSILON) *
+            np.log(next_state_max_action_index_probabilities + self.EPSILON))
 
-        penalty_info = {
-            constants.Constants.CURRENT_STATE_MAX_UNCERTAINTY: current_state_max_uncertainty,
-            constants.Constants.CURRENT_STATE_MEAN_UNCERTAINTY: current_state_mean_uncertainty,
-            constants.Constants.CURRENT_STATE_SELECT_UNCERTAINTY: current_state_select_uncertainty,
-            constants.Constants.CURRENT_STATE_POLICY_ENTROPY: current_state_policy_entropy,
-            constants.Constants.NEXT_STATE_MAX_UNCERTAINTY: next_state_max_uncertainty,
-            constants.Constants.NEXT_STATE_MEAN_UNCERTAINTY: next_state_mean_uncertainty,
-            constants.Constants.NEXT_STATE_POLICY_ENTROPY: next_state_policy_entropy,
+        return {
+            constants.Constants.CURRENT_STATE_MAX_UNCERTAINTY:
+            current_state_max_uncertainty,
+            constants.Constants.CURRENT_STATE_MEAN_UNCERTAINTY:
+            current_state_mean_uncertainty,
+            constants.Constants.CURRENT_STATE_SELECT_UNCERTAINTY:
+            current_state_select_uncertainty,
+            constants.Constants.CURRENT_STATE_POLICY_ENTROPY:
+            current_state_policy_entropy,
+            constants.Constants.NEXT_STATE_MAX_UNCERTAINTY:
+            next_state_max_uncertainty,
+            constants.Constants.NEXT_STATE_MEAN_UNCERTAINTY:
+            next_state_mean_uncertainty,
+            constants.Constants.NEXT_STATE_POLICY_ENTROPY:
+            next_state_policy_entropy,
         }
 
-        penalty = self._compute_penalty(episode=episode, penalty_info=penalty_info)
+    def __call__(self, episode, state, action, next_state, *args, **kwargs):
+        penalty_info = self._get_penalty_info(state=state,
+                                              action=action,
+                                              next_state=next_state)
+
+        penalty = self._compute_penalty(episode=episode,
+                                        penalty_info=penalty_info)
 
         return penalty, penalty_info
 
