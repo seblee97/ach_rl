b138249ac560991ab6acbf0422ed31e44b1e592a
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-02-12 21:00:45 +0000

    single cluster run method with main

--- experiments/cluster_run.py
+++ experiments/cluster_run.py
@@ -1,134 +1,64 @@
+"""
+This script is for runs being executed on the cluster.
+
+It first creates a job script with the relevant specifications, 
+and then excutes it in a subprocess.
+"""
 import argparse
-import multiprocessing
 import os
-from multiprocessing import Process
-from typing import Dict
-from typing import List
+import subprocess
 
 import constants
-from experiments import ach_config
-from experiments import run_methods
-from runners import dqn_runner
-from runners import q_learning_ensemble_runner
-from runners import q_learning_runner
-from runners import sarsa_lambda_runner
-from utils import experiment_utils
+from utils import cluster_methods
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--config_path", metavar="-C")
-parser.add_argument("--seed", metavar="-S", default=0)
-parser.add_argument("--results_folder")
-parser.add_argument("--config_changes")
-parser.add_argument("--timestamp")
-parser.add_argument("--run_name")
-parser.add_argument("--mode")
+parser.add_argument("--config_path",
+                    metavar="-C",
+                    type=str,
+                    help="path to yaml file.",
+                    required=True)
+parser.add_argument("--config_changes_paths",
+                    metavar="-CC",
+                    type=str,
+                    help="path to json config changes file.",
+                    required=False)
+parser.add_argument("--checkpoint_paths",
+                    metavar="-CP",
+                    type=str,
+                    help="path to dir in which to output results.",
+                    required=True)
 
-args = parser.parse_args()
-
-
-def parallel_run(config_path: str, seeds: List[int], results_folder: str,
-                 timestamp: str, run_name: str, changes: List[Dict]):
-    procs = []
-
-    # macOS + python 3.8 change in multiprocessing defaults.
-    # workaround: https://github.com/pytest-dev/pytest-flask/issues/104
-    multiprocessing.set_start_method("spawn")
-
-    for seed in seeds:
-        p = Process(
-            target=run_methods.single_run,
-            args=(
-                config_path,
-                results_folder,
-                timestamp,
-                run_name,
-                changes,
-                seed,
-            ),
-        )
-        p.start()
-        procs.append(p)
-
-    for p in procs:
-        p.join()
+MAIN_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def serial_run(config_path: str, seeds: List[int], results_folder: str,
-               timestamp: str, run_name: str, changes: List[Dict]):
-    for seed in seeds:
-        run_methods.single_run(config_path=config_path,
-                               seed=seed,
-                               results_folder=results_folder,
-                               timestamp=timestamp,
-                               run_name=run_name)
+def cluster_run(experiment_path: str, config_path: str, num_cpus: int,
+                memory_per_node: int) -> None:
+    """Set of experiments run in parallel.
 
+    Args:
+        config_path: path to yaml file.
+        checkpoint_paths: list of paths to directories to output results.
+    """
 
-if __name__ == '__main__':
-    try:
-        seeds = int(args.seed)
-    except ValueError:
-        seeds = [int(i) for i in args.seed.strip("[]").split(",")]
+    job_script_path = os.path.join(experiment_path,
+                                   constants.Constants.JOB_SCRIPT)
+    env = constants.Constants.ACH
+    error_path = os.path.join(experiment_path,
+                              constants.Constants.ERROR_FILE_NAME)
+    output_path = os.path.join(experiment_path,
+                               constants.Constants.OUTPUT_FILE_NAME)
 
-    config_changes = experiment_utils.json_to_config_changes(
-        args.config_changes)
+    run_command = (f"python parallel_run.py --config_path {config_path} "
+                   f"--experiment_path {experiment_path}")
 
-    print(config_changes)
+    cluster_methods.create_job_script(run_command=run_command,
+                                      save_path=job_script_path,
+                                      num_cpus=num_cpus,
+                                      conda_env_name=env,
+                                      memory=memory_per_node,
+                                      error_path=error_path,
+                                      output_path=output_path)
 
-    if isinstance(seeds, int):
-        run_methods.single_run(config_path=args.config_path,
-                               seed=seeds,
-                               results_folder=args.results_folder,
-                               timestamp=args.timestamp,
-                               run_name=args.run_name,
-                               changes=config_changes)
-    elif isinstance(seeds, list):
-        if args.mode == constants.Constants.PARALLEL:
-            parallel_run(config_path=args.config_path,
-                         seeds=seeds,
-                         results_folder=args.results_folder,
-                         timestamp=args.timestamp,
-                         run_name=args.run_name,
-                         changes=config_changes)
-        elif args.mode == constants.Constants.SERIAL:
-            serial_run(config_path=args.config_path,
-                       seeds=args.seed,
-                       results_folder=args.results_folder,
-                       timestamp=args.timestamp,
-                       run_name=args.run_name,
-                       changes=config_changes)
-
-# def single_run(config_path: str, seed: int, results_folder: str, timestamp: str, run_name: str, changes: List[Dict]):
-
-#     config = ach_config.AchConfig(config=config_path, changes=changes)
-#     checkpoint_path = experiment_utils.get_checkpoint_path(
-#         results_folder, timestamp, run_name, str(seed)
-#     )
-
-#     experiment_utils.set_random_seeds(seed)
-#     config = experiment_utils.set_device(config)
-#     config.amend_property(
-#         property_name=constants.Constants.SEED, new_property_value=seed
-#     )
-#     config.add_property(constants.Constants.EXPERIMENT_TIMESTAMP, timestamp)
-#     config.add_property(constants.Constants.CHECKPOINT_PATH, checkpoint_path)
-#     config.add_property(
-#         constants.Constants.LOGFILE_PATH,
-#         os.path.join(checkpoint_path, "data_logger.csv"),
-#     )
-
-#     os.makedirs(name=checkpoint_path, exist_ok=True)
-
-#     if config.type == constants.Constants.SARSA_LAMBDA:
-#         r = sarsa_lambda_runner.SARSALambdaRunner(config=config)
-#     elif config.type == constants.Constants.Q_LEARNING:
-#         r = q_learning_runner.QLearningRunner(config=config)
-#     elif config.type == constants.Constants.DQN:
-#         r = dqn_runner.DQNRunner(config=config)
-#     elif config.type == constants.Constants.ENSEMBLE_Q_LEARNING:
-#         r = q_learning_ensemble_runner.EnsembleQLearningRunner(config=config)
-#     else:
-#         raise ValueError(f"Learner type {type} not recognised.")
-
-#     r.train()
-#     r.post_process()
+    subprocess.call(run_command, shell=True)
+    # subprocess.call(f"qsub {job_script_path}", shell=True)
