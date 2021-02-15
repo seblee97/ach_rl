c10c124d63367fefa86b646ccdc1042012aa804b
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-02-15 22:49:45 +0000

    major: allowed for multiple branches in q network, this allows us to have an 'ensemble' of q learners that share early layers and branch of at later layers

--- learners/deep_learners/components/q_network.py
+++ learners/deep_learners/components/q_network.py
@@ -1,25 +1,26 @@
+import copy
 from typing import Any
 from typing import Dict
 from typing import List
 from typing import Tuple
 from typing import Union
 
+import constants
 import torch
 import torch.nn as nn
-
-import constants
 from utils import custom_torch_layers
 
 
 class QNetwork(nn.Module):
     """Q-value network."""
 
-    def __init__(
-        self,
-        state_dim: Tuple[int, int, int],
-        num_actions: int,
-        layer_specifications: List[Dict[str, Any]],
-    ):
+    def __init__(self,
+                 state_dim: Tuple[int, int, int],
+                 num_actions: int,
+                 layer_specifications: List[Dict[str, Any]],
+                 shared_layers: Union[List[int], None] = None,
+                 num_branches: int = 0,
+                 copy_initialisation: bool = False):
         """Class constructor.
 
         Args:
@@ -30,6 +31,10 @@ class QNetwork(nn.Module):
         self._state_dim = state_dim
         self._num_actions = num_actions
         self._layer_specifications = layer_specifications
+        self._shared_layers = shared_layers or list(
+            range(len(self._layer_specifications)))
+        self._num_branches = num_branches
+        self._copy_initialisation = copy_initialisation
 
         super().__init__()
 
@@ -37,63 +42,84 @@ class QNetwork(nn.Module):
 
     def _construct_layers(self):
         """Method to setup network architecture."""
-        self._layers = nn.ModuleList([])
+        self._core_layers = nn.ModuleList([])
+        self._branched_layers = nn.ModuleList(
+            [nn.ModuleList([]) for _ in range(self._num_branches)])
 
-        for layer_specification in self._layer_specifications:
+        for layer_index, layer_specification in enumerate(
+                self._layer_specifications):
 
-            layer_type = list(layer_specification.keys())[0]
-            layer_info = list(layer_specification.values())[0]
-            layer_nonlinearity = layer_info.get(constants.Constants.NONLINEARITY)
-
-            if layer_type == constants.Constants.CONV:
-                layer = nn.Conv2d(
-                    in_channels=layer_info[constants.Constants.IN_CHANNELS],
-                    out_channels=layer_info[constants.Constants.NUM_FILTERS],
-                    kernel_size=layer_info[constants.Constants.KERNEL_SIZE],
-                    stride=layer_info.get(constants.Constants.STRIDE, 1),
-                    padding=layer_info.get(constants.Constants.PADDING, 0),
-                )
-            elif layer_type == constants.Constants.FC:
-                layer = nn.Linear(
-                    in_features=layer_info.get(constants.Constants.IN_FEATURES),
-                    out_features=layer_info.get(
-                        constants.Constants.OUT_FEATURES, self._num_actions
-                    ),
-                )
-
-            elif layer_type == constants.Constants.FLATTEN:
-                layer = custom_torch_layers.Flatten()
+            if layer_index in self._shared_layers:
+                layer, nonlinearity = self._construct_layer(
+                    layer_specification=layer_specification)
+                self._core_layers.append(layer)
+                self._core_layers.append(nonlinearity)
             else:
-                raise ValueError(f"Layer type {layer_type} not recognised")
+                if self._copy_initialisation:
+                    layer, nonlinearity = self._construct_layer(
+                        layer_specification=layer_specification)
+                    for i in range(self._num_branches):
+                        self._branched_layers[i].append(copy.deepcopy(layer))
+                        self._branched_layers[i].append(
+                            copy.deepcopy(nonlinearity))
+                else:
+                    for i in range(self._num_branches):
+                        layer, nonlinearity = self._construct_layer(
+                            layer_specification=layer_specification)
+                        self._branched_layers[i].append(layer)
+                        self._branched_layers[i].append(nonlinearity)
 
-            if layer_nonlinearity == constants.Constants.RELU:
-                nonlinearity = nn.ReLU()
-            elif layer_nonlinearity == constants.Constants.IDENTITY:
-                nonlinearity = nn.Identity()
-            else:
-                raise ValueError(f"Non-linearity {layer_nonlinearity} not recognised")
+    def _construct_layer(
+            self, layer_specification: Dict) -> Tuple[nn.Module, nn.Module]:
+        layer_type = list(layer_specification.keys())[0]
+        layer_info = list(layer_specification.values())[0]
+        layer_nonlinearity = layer_info.get(constants.Constants.NONLINEARITY)
 
-            # initialise weights (only those with parameters)
-            if [p for p in layer.parameters()]:
-                self._initialise_weights(
-                    layer.weight,
-                    initialisation=layer_info.get(
-                        constants.Constants.WEIGHT_INITIALISATION
-                    ),
-                )
-                self._initialise_weights(
-                    layer.bias,
-                    initialisation=layer_info.get(
-                        constants.Constants.BIAS_INITIALISATION
-                    ),
-                )
+        if layer_type == constants.Constants.CONV:
+            layer = nn.Conv2d(
+                in_channels=layer_info[constants.Constants.IN_CHANNELS],
+                out_channels=layer_info[constants.Constants.NUM_FILTERS],
+                kernel_size=layer_info[constants.Constants.KERNEL_SIZE],
+                stride=layer_info.get(constants.Constants.STRIDE, 1),
+                padding=layer_info.get(constants.Constants.PADDING, 0),
+            )
+        elif layer_type == constants.Constants.FC:
+            layer = nn.Linear(
+                in_features=layer_info.get(constants.Constants.IN_FEATURES),
+                out_features=layer_info.get(constants.Constants.OUT_FEATURES,
+                                            self._num_actions),
+            )
 
-            self._layers.append(layer)
-            self._layers.append(nonlinearity)
+        elif layer_type == constants.Constants.FLATTEN:
+            layer = custom_torch_layers.Flatten()
+        else:
+            raise ValueError(f"Layer type {layer_type} not recognised")
 
-    def _initialise_weights(
-        self, layer_weights: nn.Parameter, initialisation: Union[str, None]
-    ) -> None:
+        if layer_nonlinearity == constants.Constants.RELU:
+            nonlinearity = nn.ReLU()
+        elif layer_nonlinearity == constants.Constants.IDENTITY:
+            nonlinearity = nn.Identity()
+        else:
+            raise ValueError(
+                f"Non-linearity {layer_nonlinearity} not recognised")
+
+        # initialise weights (only those with parameters)
+        if [p for p in layer.parameters()]:
+            self._initialise_weights(
+                layer.weight,
+                initialisation=layer_info.get(
+                    constants.Constants.WEIGHT_INITIALISATION),
+            )
+            self._initialise_weights(
+                layer.bias,
+                initialisation=layer_info.get(
+                    constants.Constants.BIAS_INITIALISATION),
+            )
+
+        return layer, nonlinearity
+
+    def _initialise_weights(self, layer_weights: nn.Parameter,
+                            initialisation: Union[str, None]) -> None:
         """Initialise weights of network layer according to specification.
 
         Initialisation is in-place.
@@ -112,9 +138,14 @@ class QNetwork(nn.Module):
         elif initialisation == constants.Constants.XAVIER_UNIFORM:
             nn.init.xavier_uniform_(layer_weights)
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor, branch: Union[int, None] = None):
         """Forward pass through network."""
-        for layer in self._layers:
+
+        for layer in self._core_layers:
             x = layer(x)
+
+        if branch is not None:
+            for layer in self._branched_layers[branch]:
+                x = layer(x)
 
         return x
