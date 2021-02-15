89bb90c512e423c805f7c63569e5f33d7716d605
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-02-15 18:08:47 +0000

    config changes for baseline vs hard coded vs policy entropy experiment

--- experiments/config_changes.py
+++ experiments/config_changes.py
@@ -7,12 +7,38 @@ configuration files we can use the same base for all and
 systematically modify it for each different run.
 """
 
+# CONFIG_CHANGES = {
+#     f"vp_{i}": [{
+#         "learner": {
+#             "hard_coded": {
+#                 "vp_schedule": [[0, float(i)]]
+#             }
+#         }
+#     }] for i in range(2)
+# }
+
 CONFIG_CHANGES = {
-    f"vp_{i}": [{
+    "baseline": [],
+    "hard_coded": [{
         "learner": {
             "hard_coded": {
-                "vp_schedule": [[0, float(i)]]
+                "vp_schedule": [[0, -0.025]]
             }
         }
-    }] for i in range(2)
+    }, {
+        "learner": {
+            "visitation_penalty_type": "hard_coded"
+        }
+    }],
+    "policy_entropy": [{
+        "learner": {
+            "policy_entropy_penalty": {
+                "multiplicative_factor": -0.01
+            }
+        }
+    }, {
+        "learner": {
+            "visitation_penalty_type": "policy_entropy_penalty"
+        }
+    }]
 }
