769f27730c79742d7d26544360512367a5c288a5
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-02-17 13:22:20 +0000

    network and tabular now have compue state values methods, that implement base class abstract method

--- visitation_penalties/network_visitation_penalty.py
+++ visitation_penalties/network_visitation_penalty.py
@@ -1,20 +1,17 @@
-import abc
-from typing import Any
-from typing import Dict
-from typing import List
-
-import constants
 import numpy as np
 import torch
 import torch.nn as nn
+from visitation_penalties import base_visitation_penalty
 
 
-class NetworkVisitationPenalty:
+class NetworkVisitationPenalty(base_visitation_penalty.BaseVisitationPenalty):
 
-    EPSILON = 1e-8
-
-    def __init__(self):
+    def __init__(
+        self,
+        penalty_computer: base_visitation_penalty.BaseVisitationPenaltyComputer
+    ):
         self._q_network: nn.Module
+        super().__init__(penalty_computer)
 
     @property
     def q_network(self):
@@ -24,83 +21,10 @@ class NetworkVisitationPenalty:
     def q_network(self, q_network: nn.Module):
         self._q_network = q_network
 
-    def _get_penalty_info(
-        self,
-        state,
-        action,
-        next_state,
-    ):
-        """compute series of uncertainties over states culminating in penalty."""
+    def _compute_state_values(self, state):
         with torch.no_grad():
-            current_state_values = np.array([
-                self._q_network(state,
-                                branch=i).cpu().detach().numpy().flatten()
-                for i in range(len(self._q_network._branched_layers))
-            ])
-            next_state_values = np.array([
-                self._q_network(next_state,
-                                branch=i).cpu().detach().numpy().flatten()
-                for i in range(len(self._q_network._branched_layers))
-            ])
-
-        num_actions = len(current_state_values[0])
-
-        current_state_max_action_values = np.amax(current_state_values, axis=1)
-        current_state_max_action_indices = np.argmax(current_state_values,
-                                                     axis=1)
-
-        current_state_max_uncertainty = np.std(current_state_max_action_values)
-        current_state_mean_uncertainty = np.mean(
-            np.std(current_state_values, axis=0))
-
-        current_state_select_uncertainty = np.std(current_state_values[:,
-                                                                       action])
-        current_state_max_action_index_probabilities = np.bincount(
-            current_state_max_action_indices,
-            minlength=num_actions) / len(current_state_max_action_indices)
-        current_state_policy_entropy = -np.sum(
-            (current_state_max_action_index_probabilities + self.EPSILON) *
-            np.log(current_state_max_action_index_probabilities + self.EPSILON))
-
-        next_state_max_action_values = np.max(next_state_values, axis=1)
-        next_state_max_action_indices = np.argmax(next_state_values, axis=1)
-
-        next_state_max_uncertainty = np.std(next_state_max_action_values)
-        next_state_mean_uncertainty = np.mean(np.std(next_state_values, axis=0))
-        next_state_max_action_index_probabilities = np.bincount(
-            next_state_max_action_indices,
-            minlength=num_actions) / len(current_state_max_action_indices)
-        next_state_policy_entropy = -np.sum(
-            (next_state_max_action_index_probabilities + self.EPSILON) *
-            np.log(next_state_max_action_index_probabilities + self.EPSILON))
-
-        return {
-            constants.Constants.CURRENT_STATE_MAX_UNCERTAINTY:
-                current_state_max_uncertainty,
-            constants.Constants.CURRENT_STATE_MEAN_UNCERTAINTY:
-                current_state_mean_uncertainty,
-            constants.Constants.CURRENT_STATE_SELECT_UNCERTAINTY:
-                current_state_select_uncertainty,
-            constants.Constants.CURRENT_STATE_POLICY_ENTROPY:
-                current_state_policy_entropy,
-            constants.Constants.NEXT_STATE_MAX_UNCERTAINTY:
-                next_state_max_uncertainty,
-            constants.Constants.NEXT_STATE_MEAN_UNCERTAINTY:
-                next_state_mean_uncertainty,
-            constants.Constants.NEXT_STATE_POLICY_ENTROPY:
-                next_state_policy_entropy,
-        }
-
-    def __call__(self, episode, state, action, next_state, *args, **kwargs):
-        penalty_info = self._get_penalty_info(state=state,
-                                              action=action,
-                                              next_state=next_state)
-
-        penalty = self._compute_penalty(episode=episode,
-                                        penalty_info=penalty_info)
-
-        return penalty, penalty_info
-
-    @abc.abstractmethod
-    def _compute_penalty(self, episode: int, penalty_info: Dict[str, Any]):
-        pass
+            state_values = np.array(
+                self._q_network.forward_all_heads(state).cpu().detach().numpy())
+        # output of forward_all_heads method has dimensions [NUM_LEARNERS x BATCH_SIZE x NUM_ACTIONS]
+        # for penalty compuation we have batch size of 1 and want to squash this dimension
+        return state_values.squeeze()
--- visitation_penalties/tabular_visitation_penalty.py
+++ visitation_penalties/tabular_visitation_penalty.py
@@ -1,4 +1,27 @@
-class TabularVisitationPenalty:
+from typing import Dict
+from typing import List
+from typing import Tuple
 
-    def __init__(self):
-        pass
+import numpy as np
+from visitation_penalties import base_visitation_penalty
+
+
+class TabularVisitationPenalty(base_visitation_penalty.BaseVisitationPenalty):
+
+    def __init__(
+        self,
+        penalty_computer: base_visitation_penalty.BaseVisitationPenaltyComputer
+    ):
+        super().__init__(penalty_computer)
+
+    @property
+    def state_action_values(self):
+        return self._state_action_values
+
+    @state_action_values.setter
+    def state_action_values(self, state_action_values: List[Dict[Tuple[int],
+                                                                 float]]):
+        self._state_action_values = state_action_values
+
+    def _compute_state_values(self, state):
+        return np.array([s[state] for s in self._state_action_values])
