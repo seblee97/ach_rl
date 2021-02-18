fc5b7eeb8bdf5ae70c29fadb16766c0f9bc34009
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-02-18 17:44:50 +0000

    imeplemented use of target network

--- visitation_penalties/network_visitation_penalty.py
+++ visitation_penalties/network_visitation_penalty.py
@@ -6,11 +6,13 @@ from visitation_penalties import base_visitation_penalty
 
 class NetworkVisitationPenalty(base_visitation_penalty.BaseVisitationPenalty):
 
-    def __init__(
-        self,
-        penalty_computer: base_visitation_penalty.BaseVisitationPenaltyComputer
-    ):
+    def __init__(self,
+                 penalty_computer: base_visitation_penalty.
+                 BaseVisitationPenaltyComputer,
+                 use_target_network: bool = False):
         self._q_network: nn.Module
+        self._target_q_network: nn.Module
+        self._use_target_network = use_target_network
         super().__init__(penalty_computer)
 
     @property
@@ -21,10 +23,24 @@ class NetworkVisitationPenalty(base_visitation_penalty.BaseVisitationPenalty):
     def q_network(self, q_network: nn.Module):
         self._q_network = q_network
 
+    @property
+    def target_q_network(self):
+        return self._target_q_network
+
+    @target_q_network.setter
+    def target_q_network(self, target_q_network: nn.Module):
+        self._target_q_network = target_q_network
+
     def _compute_state_values(self, state):
         with torch.no_grad():
-            state_values = np.array(
-                self._q_network.forward_all_heads(state).cpu().detach().numpy())
+            if self._use_target_network:
+                state_values = np.array(
+                    self._target_q_network.forward_all_heads(
+                        state).cpu().detach().numpy())
+            else:
+                state_values = np.array(
+                    self._q_network.forward_all_heads(
+                        state).cpu().detach().numpy())
         # output of forward_all_heads method has dimensions [NUM_LEARNERS x BATCH_SIZE x NUM_ACTIONS]
         # for penalty compuation we have batch size of 1 and want to squash this dimension
         return state_values.squeeze()
