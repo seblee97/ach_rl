8a802dca35f407453cb959fea55eb6359254749f
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-02-24 10:31:22 +0000

    penalty yranstion named tuple

--- utils/custom_objects.py
+++ utils/custom_objects.py
@@ -28,9 +28,24 @@ MaskedTransition = namedtuple(
 MaskedPenaltyTransition = namedtuple(
     typename=constants.Constants.TRANSITION,
     field_names=[
-        constants.Constants.STATE_ENCODING, constants.Constants.ACTION,
-        constants.Constants.REWARD, constants.Constants.NEXT_STATE_ENCODING,
-        constants.Constants.ACTIVE, constants.Constants.MASK,
-        constants.Constants.PENALTY
+        constants.Constants.STATE_ENCODING,
+        constants.Constants.ACTION,
+        constants.Constants.REWARD,
+        constants.Constants.NEXT_STATE_ENCODING,
+        constants.Constants.ACTIVE,
+        constants.Constants.MASK,
+        constants.Constants.PENALTY,
+    ],
+)
+
+PenaltyTransition = namedtuple(
+    typename=constants.Constants.TRANSITION,
+    field_names=[
+        constants.Constants.STATE_ENCODING,
+        constants.Constants.ACTION,
+        constants.Constants.REWARD,
+        constants.Constants.NEXT_STATE_ENCODING,
+        constants.Constants.ACTIVE,
+        constants.Constants.PENALTY,
     ],
 )
