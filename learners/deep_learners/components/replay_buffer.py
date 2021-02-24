682a375813a9abced334e4a56a3400b7a859da2b
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-02-24 10:30:42 +0000

    option of penalty ytransition

--- learners/deep_learners/components/replay_buffer.py
+++ learners/deep_learners/components/replay_buffer.py
@@ -10,48 +10,60 @@ class ReplayBuffer:
     """Object for experience replay,
     used to store experience tuples for off-policy learning."""
 
-    def __init__(self,
-                 replay_size: int,
-                 state_dim: Tuple,
-                 mask_length: Union[int, None] = None,
-                 penalties: bool = False) -> None:
+    def __init__(
+        self,
+        replay_size: int,
+        state_dim: Tuple,
+        mask_length: Union[int, None] = None,
+        penalties: bool = False,
+    ) -> None:
         """Class constructor.
 
         Args:
             replay_size: size of buffer.
             state_dim: dimension of state.
         """
-        self._states_buffer = np.zeros((replay_size,) + state_dim,
-                                       dtype=np.float32)
-        self._actions_buffer = np.zeros(replay_size, dtype=np.int8)
-        self._rewards_buffer = np.zeros(replay_size, dtype=np.float32)
-        self._next_states_buffer = np.zeros((replay_size,) + state_dim,
-                                            dtype=np.float32)
-        self._active_buffer = np.zeros(replay_size, dtype=bool)
-        if mask_length is not None:
-            self._mask_buffer = np.zeros((replay_size, mask_length),
-                                         dtype=np.int8)
-        else:
-            self._mask_buffer = None
+        self._replay_size = replay_size
+        self._state_dim = state_dim
+        self._mask_length = mask_length
         self._penalties = penalties
-        if self._penalties:
-            self._penalty_buffer = np.zeros(replay_size, dtype=np.float32)
+        self._initialise_buffers()
 
         self._insertion_index = 0
-        self._replay_size = replay_size
+
+    def _initialise_buffers(self):
+        self._states_buffer = np.zeros(
+            (self._replay_size,) + self._state_dim, dtype=np.float32
+        )
+        self._actions_buffer = np.zeros(self._replay_size, dtype=np.int8)
+        self._rewards_buffer = np.zeros(self._replay_size, dtype=np.float32)
+        self._next_states_buffer = np.zeros(
+            (self._replay_size,) + self._state_dim, dtype=np.float32
+        )
+        self._active_buffer = np.zeros(self._replay_size, dtype=bool)
+        if self._mask_length is not None:
+            self._mask_buffer = np.zeros(
+                (self._replay_size, self._mask_length), dtype=np.int8
+            )
+        else:
+            self._mask_buffer = None
+        if self._penalties:
+            self._penalty_buffer = np.zeros(self._replay_size, dtype=np.float32)
 
     @property
     def states(self) -> np.ndarray:
         return self._states_buffer
 
-    def add(self,
-            state: np.ndarray,
-            action: int,
-            reward: float,
-            next_state: np.ndarray,
-            active: bool,
-            mask: Union[np.ndarray, None] = None,
-            penalty: Union[np.ndarray, None] = None) -> None:
+    def add(
+        self,
+        state: np.ndarray,
+        action: int,
+        reward: float,
+        next_state: np.ndarray,
+        active: bool,
+        mask: Union[np.ndarray, None] = None,
+        penalty: Union[np.ndarray, None] = None,
+    ) -> None:
         """Add tuple to buffer.
 
         Args:
@@ -84,9 +96,9 @@ class ReplayBuffer:
             sample: experience sample.
         """
         current_num_entries = min(self._insertion_index, self._replay_size)
-        sample_indices = np.random.choice(current_num_entries,
-                                          size=batch_size,
-                                          replace=False)
+        sample_indices = np.random.choice(
+            current_num_entries, size=batch_size, replace=False
+        )
 
         states_sample = self._states_buffer[sample_indices]
         actions_sample = self._actions_buffer[sample_indices]
@@ -106,7 +118,8 @@ class ReplayBuffer:
                     next_state_encoding=next_states_sample,
                     active=active_sample,
                     mask=mask_sample,
-                    penalty=penalty_sample)
+                    penalty=penalty_sample,
+                )
             else:
                 return custom_objects.MaskedTransition(
                     state_encoding=states_sample,
@@ -114,13 +127,25 @@ class ReplayBuffer:
                     reward=rewards_sample,
                     next_state_encoding=next_states_sample,
                     active=active_sample,
-                    mask=mask_sample)
+                    mask=mask_sample,
+                )
 
         else:
-            return custom_objects.Transition(
-                state_encoding=states_sample,
-                action=actions_sample,
-                reward=rewards_sample,
-                next_state_encoding=next_states_sample,
-                active=active_sample,
-            )
+            if self._penalties:
+                penalty_sample = self._penalty_buffer[sample_indices]
+                return custom_objects.PenaltyTransition(
+                    state_encoding=states_sample,
+                    action=actions_sample,
+                    reward=rewards_sample,
+                    next_state_encoding=next_states_sample,
+                    active=active_sample,
+                    penalty=penalty_sample,
+                )
+            else:
+                return custom_objects.Transition(
+                    state_encoding=states_sample,
+                    action=actions_sample,
+                    reward=rewards_sample,
+                    next_state_encoding=next_states_sample,
+                    active=active_sample,
+                )
