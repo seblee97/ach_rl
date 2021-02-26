2a3a21be8550c2661d2b295e081da609e24bc4ca
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-02-26 15:14:53 +0000

    strip method replaced by replace

--- experiments/run.py
+++ experiments/run.py
@@ -18,37 +18,35 @@ MAIN_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--mode",
-                    metavar="-M",
-                    required=True,
-                    help="run experiment.")
-parser.add_argument("--config_path",
-                    metavar="-C",
-                    default="config.yaml",
-                    help="path to base configuration file.")
-parser.add_argument("--seeds",
-                    metavar="-S",
-                    default="[0]",
-                    help="list of seeds to run.")
-parser.add_argument("--config_changes",
-                    metavar="-CC",
-                    default="config_changes.py")
+parser.add_argument("--mode", metavar="-M", required=True, help="run experiment.")
+parser.add_argument(
+    "--config_path",
+    metavar="-C",
+    default="config.yaml",
+    help="path to base configuration file.",
+)
+parser.add_argument(
+    "--seeds", metavar="-S", default="[0]", help="list of seeds to run."
+)
+parser.add_argument("--config_changes", metavar="-CC", default="config_changes.py")
 parser.add_argument(
     "--memory",
     metavar="-MEM",
     default=64,
-    help="amount of memory to assign to each node when working on cluster")
-parser.add_argument("--num_gpus",
-                    default=0,
-                    help="number of gpus to use in cluster")
+    help="amount of memory to assign to each node when working on cluster",
+)
+parser.add_argument("--num_gpus", default=0, help="number of gpus to use in cluster")
 parser.add_argument(
     "--num_cpus",
     default=0,
-    help="number of cpus to use in cluster. Inferred if not given.")
+    help="number of cpus to use in cluster. Inferred if not given.",
+)
 parser.add_argument("--gpu_type", default="K80", help="type of gpu")
-parser.add_argument("--cluster_debug",
-                    action="store_true",
-                    help="leads to subprocess call and not qsub.")
+parser.add_argument(
+    "--cluster_debug",
+    action="store_true",
+    help="leads to subprocess call and not qsub.",
+)
 
 
 def _process_seed_arguments(seeds: Union[str, List[int]]):
@@ -62,10 +60,9 @@ def _process_seed_arguments(seeds: Union[str, List[int]]):
     return seeds
 
 
-def _organise_config_changes_and_checkpoint_dirs(experiment_path,
-                                                 config_changes: Dict[
-                                                     str, List[Dict]],
-                                                 seeds: List[int]) -> List[str]:
+def _organise_config_changes_and_checkpoint_dirs(
+    experiment_path, config_changes: Dict[str, List[Dict]], seeds: List[int]
+) -> List[str]:
     checkpoint_paths = []
     for run_name, changes in config_changes.items():
         for seed in seeds:
@@ -74,8 +71,10 @@ def _organise_config_changes_and_checkpoint_dirs(experiment_path,
             changes.append({constants.Constants.SEED: seed})
             experiment_utils.config_changes_to_json(
                 config_changes=changes,
-                json_path=os.path.join(checkpoint_path,
-                                       constants.Constants.CONFIG_CHANGES_JSON))
+                json_path=os.path.join(
+                    checkpoint_path, constants.Constants.CONFIG_CHANGES_JSON
+                ),
+            )
             checkpoint_paths.append(checkpoint_path)
     return checkpoint_paths
 
@@ -91,35 +90,41 @@ if __name__ == "__main__":
     os.makedirs(name=experiment_path, exist_ok=True)
 
     # logger at root of experiment i.e. not individual runs or seeds
-    logger = experiment_logger.get_logger(experiment_path=experiment_path,
-                                          name=__name__)
+    logger = experiment_logger.get_logger(
+        experiment_path=experiment_path, name=__name__
+    )
 
     if args.mode == constants.Constants.SINGLE:
-        single_checkpoint_path = os.path.join(experiment_path,
-                                              constants.Constants.SINGLE)
+        single_checkpoint_path = os.path.join(
+            experiment_path, constants.Constants.SINGLE
+        )
         os.makedirs(name=single_checkpoint_path, exist_ok=True)
-        single_run.single_run(config_path=args.config_path,
-                              checkpoint_path=single_checkpoint_path)
+        single_run.single_run(
+            config_path=args.config_path, checkpoint_path=single_checkpoint_path
+        )
     else:
         seeds = _process_seed_arguments(args.seeds)
         logger.info(f"Seeds after processing: {seeds}")
         config_changes = importlib.import_module(
-            name=f"{args.config_changes.strip('.py')}").CONFIG_CHANGES
+            name=f"{args.config_changes.replace('.py', '')}"
+        ).CONFIG_CHANGES
         experiment_utils.config_changes_to_json(
             config_changes=config_changes,
             json_path=os.path.join(
-                experiment_path,
-                f"all_{constants.Constants.CONFIG_CHANGES_JSON}"))
+                experiment_path, f"all_{constants.Constants.CONFIG_CHANGES_JSON}"
+            ),
+        )
         checkpoint_paths = _organise_config_changes_and_checkpoint_dirs(
-            experiment_path=experiment_path,
-            config_changes=config_changes,
-            seeds=seeds)
+            experiment_path=experiment_path, config_changes=config_changes, seeds=seeds
+        )
         if args.mode == constants.Constants.PARALLEL:
-            parallel_run.parallel_run(config_path=args.config_path,
-                                      checkpoint_paths=checkpoint_paths)
+            parallel_run.parallel_run(
+                config_path=args.config_path, checkpoint_paths=checkpoint_paths
+            )
         elif args.mode == constants.Constants.SERIAL:
-            serial_run.serial_run(config_path=args.config_path,
-                                  checkpoint_paths=checkpoint_paths)
+            serial_run.serial_run(
+                config_path=args.config_path, checkpoint_paths=checkpoint_paths
+            )
         elif args.mode == constants.Constants.CLUSTER:
             if args.num_cpus:
                 num_cpus = args.num_cpus
@@ -131,14 +136,17 @@ if __name__ == "__main__":
                 num_cpus = 64
             else:
                 raise ValueError(
-                    f"{len(checkpoint_paths)} is too many run combinations.")
-            cluster_run.cluster_run(experiment_path=experiment_path,
-                                    config_path=args.config_path,
-                                    num_cpus=num_cpus,
-                                    memory_per_node=args.memory,
-                                    num_gpus=args.num_gpus,
-                                    gpu_type=args.gpu_type,
-                                    cluster_debug=args.cluster_debug)
+                    f"{len(checkpoint_paths)} is too many run combinations."
+                )
+            cluster_run.cluster_run(
+                experiment_path=experiment_path,
+                config_path=args.config_path,
+                num_cpus=num_cpus,
+                memory_per_node=args.memory,
+                num_gpus=args.num_gpus,
+                gpu_type=args.gpu_type,
+                cluster_debug=args.cluster_debug,
+            )
         elif args.mode == constants.Constants.CLUSTER_ARRAY:
             raise NotADirectoryError
         else:
