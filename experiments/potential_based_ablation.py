5cc57ac36512ab719e81bcd95b79f233052db835
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-02-26 16:07:34 +0000

    specific penlaty types wtill under learner

--- experiments/potential_based_ablation.py
+++ experiments/potential_based_ablation.py
@@ -7,16 +7,24 @@ _baseline = {
 
 _hard_coded = {
     f"hard_coded_{i}": [
-        {"learner": {"visitation_penalty_type": "hard_coded"}},
-        {"hard_coded": {"vp_schedule": [[0, i]]}},
+        {
+            "learner": {
+                "visitation_penalty_type": "hard_coded",
+                "hard_coded": {"vp_schedule": [[0, i]]},
+            }
+        },
     ]
     for i in [-0.1, -0.01, -1]
 }
 
 _policy_entropy = {
     f"policy_entropy_{i}": [
-        {"learner": {"visitation_penalty_type": "policy_entropy_penalty"}},
-        {"policy_entropy_penalty": {"multiplicative_factor": i}},
+        {
+            "learner": {
+                "visitation_penalty_type": "policy_entropy_penalty",
+                "policy_entropy_penalty": {"multiplicative_factor": i},
+            }
+        },
     ]
     for i in [-0.1, -0.01, -1]
 }
@@ -25,18 +33,22 @@ _potential_policy_entropy = {
     f"potential_policy_entropy_{i}": [
         {
             "learner": {
-                "visitation_penalty_type": "potential_based_policy_entropy_penalty"
+                "visitation_penalty_type": "potential_based_policy_entropy_penalty",
+                "potential_based_policy_entropy_penalty": {"multiplicative_factor": i},
             }
         },
-        {"potential_based_policy_entropy_penalty": {"multiplicative_factor": i}},
     ]
     for i in [-0.1, -0.01, -1]
 }
 
 _adaptive_uncertainty = {
     f"uncertainty_{i}": [
-        {"learner": {"visitation_penalty_type": "adaptive_uncertainty"}},
-        {"adaptive_uncertainty": {"multiplicative_factor": i}},
+        {
+            "learner": {
+                "visitation_penalty_type": "adaptive_uncertainty",
+                "adaptive_uncertainty": {"multiplicative_factor": i},
+            }
+        },
     ]
     for i in [-0.1, -0.01, -1]
 }
@@ -45,10 +57,10 @@ _potential_based_adaptive_uncertainty = {
     f"potential_uncertainty_{i}": [
         {
             "learner": {
-                "visitation_penalty_type": "potential_based_adaptive_uncertainty"
+                "visitation_penalty_type": "potential_based_adaptive_uncertainty",
+                "potential_based_adaptive_uncertainty": {"multiplicative_factor": i},
             }
         },
-        {"potential_based_adaptive_uncertainty": {"multiplicative_factor": i}},
     ]
     for i in [-0.1, -0.01, -1]
 }
