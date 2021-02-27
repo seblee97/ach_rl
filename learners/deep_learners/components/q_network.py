4882a59c734995265f2ea1fccb495e6a44004946
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-02-27 16:20:00 +0000

    split out construct layer method

--- learners/deep_learners/components/q_network.py
+++ learners/deep_learners/components/q_network.py
@@ -38,57 +38,64 @@ class QNetwork(nn.Module):
         """Method to setup network architecture."""
         self._layers = nn.ModuleList([])
 
-        for layer_specification in self._layer_specifications:
+        for layer_index, layer_specification in enumerate(self._layer_specifications):
 
-            layer_type = list(layer_specification.keys())[0]
-            layer_info = list(layer_specification.values())[0]
-            layer_nonlinearity = layer_info.get(constants.Constants.NONLINEARITY)
-
-            if layer_type == constants.Constants.CONV:
-                layer = nn.Conv2d(
-                    in_channels=layer_info[constants.Constants.IN_CHANNELS],
-                    out_channels=layer_info[constants.Constants.NUM_FILTERS],
-                    kernel_size=layer_info[constants.Constants.KERNEL_SIZE],
-                    stride=layer_info.get(constants.Constants.STRIDE, 1),
-                    padding=layer_info.get(constants.Constants.PADDING, 0),
-                )
-            elif layer_type == constants.Constants.FC:
-                layer = nn.Linear(
-                    in_features=layer_info.get(constants.Constants.IN_FEATURES),
-                    out_features=layer_info.get(
-                        constants.Constants.OUT_FEATURES, self._num_actions
-                    ),
-                )
-
-            elif layer_type == constants.Constants.FLATTEN:
-                layer = custom_torch_layers.Flatten()
-            else:
-                raise ValueError(f"Layer type {layer_type} not recognised")
-
-            if layer_nonlinearity == constants.Constants.RELU:
-                nonlinearity = nn.ReLU()
-            elif layer_nonlinearity == constants.Constants.IDENTITY:
-                nonlinearity = nn.Identity()
-            else:
-                raise ValueError(f"Non-linearity {layer_nonlinearity} not recognised")
-
-            # initialise weights (only those with parameters)
-            if [p for p in layer.parameters()]:
-                self._initialise_weights(
-                    layer.weight,
-                    initialisation=layer_info.get(
-                        constants.Constants.WEIGHT_INITIALISATION
-                    ),
-                )
-                self._initialise_weights(
-                    layer.bias,
-                    initialisation=layer_info.get(
-                        constants.Constants.BIAS_INITIALISATION
-                    ),
-                )
+            layer, nonlinearity = self._construct_layer(
+                layer_specification=layer_specification
+            )
 
             self._layers.append(layer)
             self._layers.append(nonlinearity)
+
+    def _construct_layer(
+        self, layer_specification: Dict
+    ) -> Tuple[nn.Module, nn.Module]:
+        layer_type = list(layer_specification.keys())[0]
+        layer_info = list(layer_specification.values())[0]
+        layer_nonlinearity = layer_info.get(constants.Constants.NONLINEARITY)
+
+        if layer_type == constants.Constants.CONV:
+            layer = nn.Conv2d(
+                in_channels=layer_info[constants.Constants.IN_CHANNELS],
+                out_channels=layer_info[constants.Constants.NUM_FILTERS],
+                kernel_size=layer_info[constants.Constants.KERNEL_SIZE],
+                stride=layer_info.get(constants.Constants.STRIDE, 1),
+                padding=layer_info.get(constants.Constants.PADDING, 0),
+            )
+        elif layer_type == constants.Constants.FC:
+            layer = nn.Linear(
+                in_features=layer_info.get(constants.Constants.IN_FEATURES),
+                out_features=layer_info.get(
+                    constants.Constants.OUT_FEATURES, self._num_actions
+                ),
+            )
+
+        elif layer_type == constants.Constants.FLATTEN:
+            layer = custom_torch_layers.Flatten()
+        else:
+            raise ValueError(f"Layer type {layer_type} not recognised")
+
+        if layer_nonlinearity == constants.Constants.RELU:
+            nonlinearity = nn.ReLU()
+        elif layer_nonlinearity == constants.Constants.IDENTITY:
+            nonlinearity = nn.Identity()
+        else:
+            raise ValueError(f"Non-linearity {layer_nonlinearity} not recognised")
+
+        # initialise weights (only those with parameters)
+        if [p for p in layer.parameters()]:
+            self._initialise_weights(
+                layer.weight,
+                initialisation=layer_info.get(
+                    constants.Constants.WEIGHT_INITIALISATION
+                ),
+            )
+            self._initialise_weights(
+                layer.bias,
+                initialisation=layer_info.get(constants.Constants.BIAS_INITIALISATION),
+            )
+
+        return layer, nonlinearity
 
     def _initialise_weights(
         self, layer_weights: nn.Parameter, initialisation: Union[str, None]
