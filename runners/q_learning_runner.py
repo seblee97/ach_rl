1607ed8969b9fc20c90791b3d2f3671b49d9a3d2
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-06-03 19:02:14 +0100

    add abstract mehtod post visuatlisaion, visualisations

--- runners/q_learning_runner.py
+++ runners/q_learning_runner.py
@@ -7,6 +7,8 @@ from typing import Tuple
 
 import constants
 
+import os
+
 
 class QLearningRunner(base_runner.BaseRunner):
     """Runner for vanilla Q-learning."""
@@ -29,13 +31,43 @@ class QLearningRunner(base_runner.BaseRunner):
         return learner
 
     def _pre_episode_log(self, episode: int):
+        visualisation_configurations = [
+            (constants.Constants.MAX_VALUES_PDF, True, False),
+            (constants.Constants.QUIVER_VALUES_PDF, False, True),
+            (constants.Constants.QUIVER_MAX_VALUES_PDF, True, True),
+        ]
         if self._visualisation_iteration(constants.Constants.VALUE_FUNCTION, episode):
-            pass
+            for visualisation_configuration in visualisation_configurations:
+                self._logger.info(
+                    "Serial value function visualisation: "
+                    f"{visualisation_configuration[0]}"
+                )
+                self._environment.plot_value_function(
+                    values=self._learner.state_action_values,
+                    save_path=os.path.join(
+                        self._visualisations_folder_path,
+                        f"{episode}_{visualisation_configuration[0]}",
+                    ),
+                    plot_max_values=visualisation_configuration[1],
+                    quiver=visualisation_configuration[2],
+                    over_actions=constants.Constants.MAX,
+                )
+        if self._visualisation_iteration(constants.Constants.VISITATION_COUNT_HEATMAP, episode):
+            self._environment.plot_value_function(
+                    values=self._learner.state_visitation_counts,
+                    save_path=os.path.join(
+                        self._visualisations_folder_path,
+                        f"{episode}_{constants.Constants.VISITATION_COUNT_HEATMAP_PDF}",
+                    ),
+                    plot_max_values=True,
+                    quiver=False,
+                    over_actions=constants.Constants.SELECT,
+                )
         if episode != 0:
             if self._visualisation_iteration(
                 constants.Constants.INDIVIDUAL_TRAIN_RUN, episode
             ):
-                self._logger.plot_array_data(
+                self._data_logger.plot_array_data(
                     name=f"{constants.Constants.INDIVIDUAL_TRAIN_RUN}_{episode}",
                     data=self._environment.plot_episode_history(),
                 )
@@ -63,20 +95,25 @@ class QLearningRunner(base_runner.BaseRunner):
         """
         episode_reward = 0
 
-        visitation_penalty = self._visitation_penalty(episode)
-
         state = self._environment.reset_environment(train=True)
+
+        self._visitation_penalty.state_action_values = [self._learner.state_action_values]
 
         while self._environment.active:
             action = self._learner.select_behaviour_action(state)
             reward, new_state = self._environment.step(action)
+
+            penalty, penalty_info = self._visitation_penalty(
+                episode=episode, state=state, action=action, next_state=new_state
+            )
+
             self._learner.step(
                 state,
                 action,
                 reward,
                 new_state,
                 self._environment.active,
-                visitation_penalty,
+                penalty,
             )
             state = new_state
             episode_reward += reward
@@ -85,4 +122,7 @@ class QLearningRunner(base_runner.BaseRunner):
 
     def _run_specific_tests(self, episode: int):
         """Implement specific test runs for each runner."""
+        pass
+
+    def _post_visualisation(self):
         pass
