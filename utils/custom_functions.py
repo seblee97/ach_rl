162004f847dabf24244b066cbd2e7f9e229c5544
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-08-29 23:06:06 +0100

    rgb to grayscale util function

--- utils/custom_functions.py
+++ utils/custom_functions.py
@@ -11,3 +11,9 @@ def policy_entropy(state_max_action_indices: np.ndarray, num_actions: int):
         * np.log(state_max_action_index_probabilities + constants.Constants.LOG_EPSILON)
     )
     return entropy
+
+
+def rgb_to_grayscale(rgb: np.ndarray) -> np.ndarray:
+	# rgb channel last
+	grayscale = np.dot(rgb[..., :3], [[0.299], [0.587], [0.114]])
+	return grayscale
