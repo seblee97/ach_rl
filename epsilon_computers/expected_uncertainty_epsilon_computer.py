6bff280f09aa39d093fd7a0413911212233f958f
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-09-23 13:18:38 +0100

    initial commit of epsilon computers including expected and unexpected unvertainty

---
+++ epsilon_computers/expected_uncertainty_epsilon_computer.py
@@ -0,0 +1,23 @@
+from typing import Any
+from typing import Dict
+from typing import Union
+
+import constants
+from epsilon_computers import base_epsilon_computer
+
+
+class ExpectedUncertaintyEpsilonComputer(
+        base_epsilon_computer.BaseEpsilonComputer):
+    """Epsilon tuned to 'expected' uncertainty over an ensemble."""
+
+    def __init__(self, action_function: str, minimum_value: Union[int, float]):
+        self._minimum_value = minimum_value
+        self._action_function = action_function
+
+    def _compute_epsilon(self, episode: int, epsilon_info: Dict[str, Any]):
+        if self._action_function == constants.Constants.MAX:
+            computed_epsilon = 1 - 1 / epsilon_info[constants.Constants.CURRENT_STATE_MAX_UNCERTAINTY]
+        elif self._action_function == constants.Constants.MEAN:
+            computed_epsilon = 1 - 1 / epsilon_info[constants.Constants.CURRENT_STATE_MEAN_UNCERTAINTY]
+
+        return np.amax([computed_epsilon, self._minimum_value])
---
+++ epsilon_computers/unexpected_uncertainty_epsilon_computer.py
@@ -0,0 +1,40 @@
+import collections
+from typing import Any
+from typing import Dict
+from typing import Union
+
+import constants
+import numpy as np
+from epsilon_computers import base_epsilon_computer
+
+
+class UnexpectedUncertaintyEpsilonComputer(
+        base_epsilon_computer.BaseEpsilonComputer):
+    """Epsilon tuned to 'unexpected' uncertainty over an ensemble."""
+
+    def __init__(self, moving_average_window: int, action_function: str, minimum_value: Union[int, float]):
+        self._moving_average_window = moving_average_window
+        self._action_function = action_function
+        self._minimum_value = minimum_value
+
+        self._policy_entropy_history = {}
+
+    def _compute_epsilon(self, episode: int, epsilon_info: Dict[str, Any]):
+        state = epsilon_info[constants.Constants.STATE]
+        if state not in self._policy_entropy_history:
+            self._policy_entropy_history[state] = collections.deque(
+                maxlen=self._moving_average_window
+            )
+            # fill with single element to avoid NaN.
+            self._policy_entropy_history[state].append(0)
+
+        previous_moving_average = np.mean(self._policy_entropy_history[state])
+
+        self._policy_entropy_history[state].append(epsilon_info[constants.Constants.NORMALISED_POLICY_ENTROPY])
+
+        new_moving_average = np.mean(self._policy_entropy_history[state])
+        moving_average_difference = new_moving_average - previous_moving_average
+        
+        computed_epsilon = abs(moving_average_difference)
+
+        return np.amax([computed_epsilon, self._minimum_value])
