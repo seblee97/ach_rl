da129d0b567abdca36192a1fda21d8a9864fa38e
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-10-25 12:50:24 +0100

    formatting

--- experiments/run_modes/serial_run.py
+++ experiments/run_modes/serial_run.py
@@ -19,21 +19,23 @@ from utils import experiment_utils
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--config_path",
-                    metavar="-C",
-                    type=str,
-                    help="path to yaml file.",
-                    required=True)
-parser.add_argument("--config_changes_paths",
-                    metavar="-CC",
-                    type=str,
-                    help="path to json config changes file.",
-                    required=False)
-parser.add_argument("--checkpoint_paths",
-                    metavar="-CP",
-                    type=str,
-                    help="path to dir in which to output results.",
-                    required=True)
+parser.add_argument(
+    "--config_path", metavar="-C", type=str, help="path to yaml file.", required=True
+)
+parser.add_argument(
+    "--config_changes_paths",
+    metavar="-CC",
+    type=str,
+    help="path to json config changes file.",
+    required=False,
+)
+parser.add_argument(
+    "--checkpoint_paths",
+    metavar="-CP",
+    type=str,
+    help="path to dir in which to output results.",
+    required=True,
+)
 
 MAIN_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -47,20 +49,23 @@ def serial_run(config_path: str, checkpoint_paths: List[str]) -> None:
     """
     for checkpoint_path in checkpoint_paths:
         changes = experiment_utils.json_to_config_changes(
-            os.path.join(checkpoint_path,
-                         constants.Constants.CONFIG_CHANGES_JSON))
-        single_run.single_run(config_path=config_path,
-                              checkpoint_path=checkpoint_path,
-                              changes=changes)
+            os.path.join(checkpoint_path, constants.Constants.CONFIG_CHANGES_JSON)
+        )
+        single_run.single_run(
+            config_path=config_path, checkpoint_path=checkpoint_path, changes=changes
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parser.parse_args()
     if args.config_changes_path is not None:
         config_changes = experiment_utils.json_to_config_changes(
-            args.config_changes_path)
+            args.config_changes_path
+        )
     else:
         config_changes = []
-    single_run(config_path=args.config_path,
-               checkpoint_path=args.checkpoint_path,
-               changes=config_changes)
+    single_run(
+        config_path=args.config_path,
+        checkpoint_path=args.checkpoint_path,
+        changes=config_changes,
+    )
