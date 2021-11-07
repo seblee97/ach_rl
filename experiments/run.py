a30cae424c0b23b02fa3bdf903e998e14fda29cc
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-11-07 16:54:50 +0000

    cluster mode

--- experiments/run.py
+++ experiments/run.py
@@ -3,10 +3,12 @@ import os
 
 import constants
 from experiments import ach_config
+from run_modes import cluster_run
 from run_modes import parallel_run
 from run_modes import serial_run
 from run_modes import single_run
 from run_modes import utils
+from runners import q_learning_ensemble_runner
 from runners import q_learning_runner
 
 MAIN_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -32,11 +34,24 @@ if __name__ == "__main__":
 
     results_folder = os.path.join(MAIN_FILE_PATH, constants.RESULTS)
 
+    config_class_name = "AChConfig"
+    config_module_name = "ach_config"
+    config_module_path = os.path.join(MAIN_FILE_PATH, "ach_config.py")
     config_class = ach_config.AchConfig
     config = config_class(args.config_path)
 
     if config.type == constants.Q_LEARNING:
         runner_class = q_learning_runner.QLearningRunner
+        runner_class_name = "QLearningRunner"
+        runner_module_name = "q_learning_runner"
+        runner_module_path = os.path.join(MAIN_FILE_PATH, "q_learning_runner.py")
+    elif config.type == constants.ENSEMBLE_Q_LEARNING:
+        runner_class = q_learning_ensemble_runner.EnsembleQLearningRunner
+        runner_class_name = "EnsembleQLearningRunner"
+        runner_module_name = "q_learning_ensemble_runner"
+        runner_module_path = os.path.join(
+            MAIN_FILE_PATH, "q_learning_ensemble_runner.py"
+        )
 
     if args.mode == constants.SINGLE:
 
@@ -52,47 +67,56 @@ if __name__ == "__main__":
             run_methods=["train", "post_process"],
         )
 
-    elif args.mode == constants.PARALLEL:
+    elif args.mode in [constants.PARALLEL, constants.SERIAL, constants.CLUSTER]:
 
         seeds = utils.process_seed_arguments(args.seeds)
 
         checkpoint_paths = utils.setup_experiment(
-            mode="parallel",
+            mode="multi",
             results_folder=results_folder,
             config_path=args.config_path,
             config_changes_path=args.config_changes,
             seeds=seeds,
         )
 
-        parallel_run.parallel_run(
-            runner_class=runner_class,
-            config_class=config_class,
-            config_path=args.config_path,
-            checkpoint_paths=checkpoint_paths,
-            run_methods=["train", "post_process"],
-            stochastic_packages=["numpy", "torch", "random"],
-        )
+        if args.mode == constants.PARALLEL:
 
-    elif args.mode == constants.SERIAL:
+            parallel_run.parallel_run(
+                runner_class=runner_class,
+                config_class=config_class,
+                config_path=args.config_path,
+                checkpoint_paths=checkpoint_paths,
+                run_methods=["train", "post_process"],
+                stochastic_packages=["numpy", "torch", "random"],
+            )
 
-        seeds = utils.process_seed_arguments(args.seeds)
+        elif args.mode == constants.SERIAL:
 
-        checkpoint_paths = utils.setup_experiment(
-            mode="serial",
-            results_folder=results_folder,
-            config_path=args.config_path,
-            config_changes_path=args.config_changes,
-            seeds=seeds,
-        )
+            serial_run.serial_run(
+                runner_class=runner_class,
+                config_class=config_class,
+                config_path=args.config_path,
+                checkpoint_paths=checkpoint_paths,
+                run_methods=["train", "post_process"],
+                stochastic_packages=["numpy", "torch", "random"],
+            )
 
-        serial_run.serial_run(
-            runner_class=runner_class,
-            config_class=config_class,
-            config_path=args.config_path,
-            checkpoint_paths=checkpoint_paths,
-            run_methods=["train", "post_process"],
-            stochastic_packages=["numpy", "torch", "random"],
-        )
+        elif args.mode == constants.CLUSTER:
+
+            cluster_run.cluster_run(
+                runner_class_name=runner_class_name,
+                runner_module_name=runner_module_name,
+                runner_module_path=runner_module_path,
+                config_class_name=config_class_name,
+                config_module_name=config_module_name,
+                config_module_path=config_module_path,
+                config_path=args.config_path,
+                checkpoint_paths=checkpoint_paths,
+                run_methods=["train", "post_process"],
+                stochastic_packages=["numpy", "torch", "random"],
+                env_name="rama",
+                scheduler="univa",
+            )
 
     else:
         raise ValueError(f"run mode {args.mode} not recognised.")
