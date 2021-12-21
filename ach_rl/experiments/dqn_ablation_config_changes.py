ab7b334b82ea4f763053f1d88348dfb810a72afd
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-12-21 22:35:13 +0100

    string hash

--- ach_rl/experiments/dqn_ablation_config_changes.py
+++ ach_rl/experiments/dqn_ablation_config_changes.py
@@ -60,7 +60,7 @@ key_combos = itertools.product(
 )
 
 CONFIG_CHANGES = {
-    hash("_".join(key_combo)): num_learner_changes[key_combo[0]]
+    str(hash("_".join(key_combo))): num_learner_changes[key_combo[0]]
     + shared_layer_changes[key_combo[1]]
     + mask_probability_changes[key_combo[2]]
     + buffer_size_changes[key_combo[3]]
