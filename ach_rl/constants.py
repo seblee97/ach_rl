9751408a504088b42a5ff8830209173105e88fe9
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-10-03 13:53:57 +0100

    single modes

--- ach_rl/constants.py
+++ ach_rl/constants.py
@@ -357,6 +357,10 @@ MAX_VALUE_PERCENTILE = "max_value_percentile"
 WILL_POSNER = "will_posner"
 STANDARD = "standard"
 FORMAT = "format"
+SINGLE_PARALLEL = "single_parallel"
+SINGLE_SERIAL = "single_serial"
+SINGLE_CLUSTER = "single_cluster"
+WEINAN = "weinan"
 
 ATARI_ENVS = ["PongNoFrameskip-v4", "GravitarNoFrameskip-v4"]
 
